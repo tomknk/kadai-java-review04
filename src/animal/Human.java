988cc35976c5658f0c49c948fb4e42d951d9a440
tomknk <tomita.kanako.feb@gmail.com>
2023-11-27 23:09:32 +0900

    チェックいただいた、引数ありのコンストラクタ　の修正とインデントの整形をしました。

--- src/animal/Human.java
+++ src/animal/Human.java
@@ -2,30 +2,32 @@ package animal;
 
 class Human extends Animal implements Thinkable {
 
-        private String hobby;// 趣味
+    private String hobby;// 趣味
 
-        // getter
-        public String getHobby() {
-            return hobby;
-        }
-        // setter
-        public void setHobby(String hobby) {
-            this.hobby = hobby;
-        }
-        // 引数なしのコンストラクタ
-        public Human() {
-        }
-        // 引数ありコンストラクタ
-        public Human(String Animalname, int age,String hobby) {
-          super.setAnimalname(Animalname);
-          super.setAge(age);
-          this.hobby = hobby;
-        }
+    // getter
+    public String getHobby() {
+        return hobby;
+    }
 
-        //メソッド
-        @Override
-        // thinkメソッド
-        public void think() {
-            System.out.println("私は" + hobby + "について考えています。");
-        }
+    // setter
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    // 引数なしのコンストラクタ
+    public Human() {
+    }
+
+    // 引数ありコンストラクタ
+    public Human(String Animalname, int age, String hobby) {
+        super(Animalname,age);
+        this.hobby = hobby;
+    }
+
+    // メソッド
+    @Override
+    // thinkメソッド
+    public void think() {
+        System.out.println("私は" + hobby + "について考えています。");
+    }
 }
